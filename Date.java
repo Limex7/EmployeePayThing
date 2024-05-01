1ed1eb948a189590387054f4709a8304399585e0
Limex7 <126993571+Limex7@users.noreply.github.com>
2024-04-30 20:05:55 -0700

    Update Date.java
    added some checks if values were invalid

--- Date.java
+++ Date.java
@@ -2,9 +2,19 @@ public class Date {
     private int year;
     private int month;
     private int day;
-    public Date(int month, int day, int year){ //add some checks
-        this.year = year;
+    public Date(int month, int day, int year){
+        if (year < 0){
+            throw new IllegalArgumentException("year is to low");
+        }
+        if (0 > month || month > 12){
+            throw new IllegalArgumentException("month is wrong");
+        }
         this.month = month;
+        int[] monLengths = {31,28,31,30,31,30,31,31,30,31,30,31};
+        if (day > monLengths[month-1] || day < 0){
+            throw new IllegalArgumentException("day is wrong");
+        }
+        this.year = year;
         this.day = day;
     }
     public void setYear(int Year){
